6e51092c9269e338e94cfee7b2070f7c06560cd1
roadtodiploma <roadtodiploma@roadtodiplomas-MacBook-Pro.local>
2020-03-05 11:14:31 +0100

    forcast code commented

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,3 +1,21 @@
+// const request = require('request')
+
+// const forecast = (latitude, longitude , callback) => {
+//     const url = 'https://api.darksky.net/forecast/9d70cec25b0db4d8ce5239ac6462504d/' + latitude + ',' + longitude + '?units=si'
+
+//     request ({ url: url, json: true}, (error, { body }) => {
+//         if (error) {
+//             callback ('Unable to connect to the weather service!', undefined)
+//         } else if (body.error) {
+//             callback ('Unable to find location!' , undefined)
+//         } else {
+//             callback (undefined, 'The high temperatur for today is ' + body.daily.data[0].temperatureHigh + ' degrees and low temperatur ' + body.daily.data[0].temperatureLow + ' degrees. ' + body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degresss out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+//         }
+//     })
+// }
+// module.exports = forecast
+
+
 const request = require('request')
 
 const forecast = (latitude, longitude , callback) => {
